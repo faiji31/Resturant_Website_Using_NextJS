1efe111a49a8b4a73f9ef38f4bb880878db923ed
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-05-09 15:39:59 +0600

    24th commit

--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -4,3 +4,24 @@ export async function GET(request) {
 
     return Response.json(feedback)
 }
+
+export async function POST(request) {
+
+
+    const {message} = await request.json()
+
+    if (!message| typeof message !== "string"){
+         return Response.json({
+        status:400,
+        message: " Its working"
+    })
+    }
+
+    const newfeedback = {message, id:feedback.length +1}
+    feedback.push(newfeedback)
+
+    return Response.json({
+        acknowledged: true,
+        insertedId: newfeedback.id
+    })
+}
